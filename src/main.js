35016e1b82ea14a933bdf1f36f7c46c1c29824fc
ohanhi <ossi.hanhinen@gmail.com>
2015-08-03 21:17:17 +0300

    Use button increment/decrement base

--- src/main.js
+++ src/main.js
@@ -2,12 +2,26 @@ import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
 import _hh from 'hyperscript-helpers';
 
-const { div, span, h1, a } = _hh(CycleDOM.h);
+const { div, span, h1, a, button } = _hh(CycleDOM.h);
 
-const main = () => ({
-  DOM: Cycle.Rx.Observable.interval(1000)
-    .map(i => h1(i + ' seconds elapsed'))
-});
+const main = ({DOM}) => {
+
+  let action$ = Cycle.Rx.Observable.merge(
+    DOM.get('#decrement', 'click').map(ev => -1),
+    DOM.get('#increment', 'click').map(ev => +1)
+  );
+  let count$ = action$.startWith(0).scan((x,y) => x+y);
+  return {
+    DOM: count$.map(count =>
+        div([
+          button({id: 'decrement'}, 'Decrement'),
+          button({id: 'increment'}, 'Increment'),
+          h1('Counter: ' + count)
+        ])
+      )
+  };
+
+};
 
 const drivers = {
   DOM: CycleDOM.makeDOMDriver('#app')
