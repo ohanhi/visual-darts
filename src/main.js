4214a253b392d37752edfc21eeff894d0e965ae5
ohanhi <ossi.hanhinen@gmail.com>
2015-08-03 20:58:17 +0300

    Use hyperscript-helpers

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
+import _hh from 'hyperscript-helpers';
+
+const { div, span, h1, a } = _hh(CycleDOM.h);
 
 const main = () => ({
   DOM: Cycle.Rx.Observable.interval(1000)
-    .map(i => CycleDOM.h(
-      'h1', '' + i + ' seconds elapsed'
-    ))
+    .map(i => h1(i + ' seconds elapsed'))
 });
 
 const drivers = {
